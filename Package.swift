52513fa71552c83782ab8e72fefdc8d3d13780c4
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2022-10-24 21:33:42 +0100

    chore: updated swift build files.

--- Package.swift
+++ Package.swift
@@ -5,8 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AccessibilityFocused",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -23,9 +24,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AccessibilityFocused",
-            dependencies: []),
+            dependencies: [],
+            exclude: [
+                "Example"
+            ]),
         .testTarget(
             name: "AccessibilityFocusedTests",
-            dependencies: ["AccessibilityFocused"]),
+            dependencies: ["AccessibilityFocused"],
+            exclude: [
+                "Example"
+            ]),
     ]
 )
